0de678a3be2d8bac3944c58dd1925314a88e1799
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2026-06-18 11:46:38 -0300

    Exemplos DO..WHILE

--- l051q01.js
+++ l051q01.js
@@ -1,7 +1,6 @@
 // Desenvolver um programa que exiba os números de 1 a 100.
 
 let cont = 1;  // contador
-
 while ( cont <= 100 ) {
     console.log ( cont );
     cont = cont + 1; // incremento do contador
