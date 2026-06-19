0a64a7b9ff43deb38aaa98a155d45770cfddadb0
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2026-06-19 10:20:55 -0300

    Exemplos FOR

---
+++ l052q04.js
@@ -0,0 +1,17 @@
+// Desenvolver um programa que apresente os resultados de uma tabuada 
+// de um número n qualquer a ser perguntado ao usuário 
+// (n x 1, n x 2, n x 3, ... , n x 9, n x 10)
+
+let n ;
+let cont = 0; 
+let mult ;
+
+console.log("Informe um número: ");
+n = 5;
+console.log(`Número informado: ${n}`);
+
+do {
+    mult = n * cont;
+    console.log(`${n} x ${cont} = ${mult}`);
+    cont = cont + 1; // outras opções: cont += 1 , cont ++
+} while ( cont <= 10 );
---
+++ l053q02.ts
@@ -0,0 +1,16 @@
+// Desenvolver um programa que apresente o total da 
+// soma de n números inteiros do número 1 até n, 
+// onde n é um valor informado pelo usuário.
+
+let n: number ;
+let acum: number = 0;
+
+console.log("Informe u número: ");
+n = 7;
+console.log(`Número inserido: ${n}`);
+
+for (let cont: number = 1; cont <= n; cont ++ ) {
+    acum = acum + cont;
+}
+
+console.log(`A soma dos valores de 1 a ${n} é ${acum}`);
